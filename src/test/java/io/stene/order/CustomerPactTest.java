6c734b03a966165d5f13c194899ca2ffd9642ea3
Henrik Stene <hstene@me.com>
2018-06-12 00:13:25 +0200

    Update test

--- src/test/java/io/stene/order/CustomerPactTest.java
+++ src/test/java/io/stene/order/CustomerPactTest.java
@@ -81,7 +81,7 @@ public class CustomerPactTest {
                         .body(new PactDslJsonBody()
                                 .stringValue("name", customer.getName())
                                 .stringValue("ssn", customer.getSsn())
-                                .stringValue("address", customer.getAddress())
+                                .stringType("address", customer.getAddress())
                                 .integerType("id", 0)
                         )
                 .toPact();
