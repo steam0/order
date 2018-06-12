f4d7bc35f9e836d473ac2ba6d60577461232a672
Henrik Stene <hstene@me.com>
2018-06-12 02:39:54 +0200

    Updated first test

--- src/test/java/io/stene/order/CustomerIntegrationTest.java
+++ src/test/java/io/stene/order/CustomerIntegrationTest.java
@@ -24,7 +24,6 @@ public class CustomerIntegrationTest {
 	public void createCustomer() {
 		Customer customer = Customer.builder().name("Harald Hårfagre").ssn("71039012345").address("6850 Correct street").build();
 		Customer response = customerClient.createCustomer(customer);
-
 		assertEquals(response.getName(), customer.getName());
 		assertEquals(response.getSsn(), customer.getSsn());
 		assertEquals(response.getAddress(), customer.getAddress());
